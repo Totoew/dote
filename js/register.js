97c16e269e52f2cefa4bc68a0e8993057ac108e4
Laert <nlovnas@mail.ru>
2024-12-12 22:24:13 +0500

    F

--- js/register.js
+++ js/register.js
@@ -1,6 +1,6 @@
 const task = document.querySelector('.task-card').querySelector('.name-task');
 
-window.Telegram.WebApp.ready().then(() => {
+window.Telegram.WebApp.ready.then(() => {
     const userId = window.tg.user.id;
   
     fetch('https://laert.pythonanywhere.com/register', {
