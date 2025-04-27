b101e0066f5a38bf2acd1274910275c2f0afecf2
Danil Totoev <totoevdanil57@gmail.com>
2025-04-27 19:11:06 +0500

    Для страницы настроек остался единый css-файл для обоих тем

--- js/theme-change.js
+++ js/theme-change.js
@@ -1,40 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const themeStyle = document.getElementById('theme-style');
-    const href = themeStyle.getAttribute('href');
-    const numberCSSfile = href.charAt(href.length - 5);
-    const themeToggle = document.getElementById('theme-toggle');
+document.addEventListener("DOMContentLoaded", function() {
     const body = document.body;
+    const themeToggle = document.getElementById('theme-toggle');
+    
+    const savedTheme = localStorage.getItem('theme') || 'dark';
+    body.setAttribute('data-theme', savedTheme);
+    themeToggle.checked = savedTheme === 'dark';
+    
+function updateTheme() {
+    const currentTheme = body.getAttribute('data-theme');
 
-    console.log('Проверка. Номер CSS-файла: ', numberCSSfile);
+    document.querySelectorAll('[data-light][data-dark]').forEach(img => {
+        img.src = currentTheme === 'light' 
+        ? img.getAttribute('data-light') 
+        : img.getAttribute('data-dark');
+    });
 
-    function setTheme(theme) {
-        if (theme === "light") {
-            themeStyle.setAttribute('href', `css/light-style-${numberCSSfile}.css`);
-            body.setAttribute('data-theme', 'light');
-        } else {
-            themeStyle.setAttribute('href', `css/style-${numberCSSfile}.css`);
-            body.setAttribute('data-theme', 'dark');
-        }
-        localStorage.setItem('theme', theme); 
 
-        //Обновляем изображения при смене темы
-        document.querySelectorAll('[data-light][data-dark]').forEach(img => {
-            img.src = theme === 'light' ? img.getAttribute('data-light') : img.getAttribute('data-dark');
-        });
+    const themeSwitcherText = document.querySelector('.theme-block:last-child .theme-text');
+    if (themeSwitcherText) {
+        themeSwitcherText.textContent = currentTheme === 'light' 
+        ? 'Тёмная тема' 
+        : 'Светлая тема';
     }
-
-    //Проверяем сохранённую тему при загрузке страницы
-    const savedTheme = localStorage.getItem('theme') || 'dark'; 
-    setTheme(savedTheme);
-
-    //Функция для смены темы
-    function toggleTheme() {
-        const currentTheme = body.getAttribute('data-theme') === 'light' ? 'dark' : 'light';
-        themeToggle.checked = false;
-        setTheme(currentTheme);
-    }
-
-    themeToggle.addEventListener('click', toggleTheme);
+}
+    
+function toggleTheme() {
+    const currentTheme = themeToggle.checked ? 'dark' : 'light';
+    body.setAttribute('data-theme', currentTheme);
+    localStorage.setItem('theme', currentTheme);
+    updateTheme();
+}
+    
+themeToggle.addEventListener('change', toggleTheme);
+updateTheme();
 });
-
-
